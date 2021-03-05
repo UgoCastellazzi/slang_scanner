adeae5410e1b26b8833758fe5a1e982a7bb9b420
Ugo Castellazi <ugo.castellazzi@gmail.com>
2021-03-05 12:50:12 +0100

    first home version with basic search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
+    @words = Word.all
+    if params[:word]
+      @words = @words.select { |word| word.name.start_with?(params[:word]) }
+    end
   end
 end
