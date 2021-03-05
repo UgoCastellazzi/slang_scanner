8758c90f686283342d2cb1952fce6f95bd1491ab
Ugo Castellazi <ugo.castellazzi@gmail.com>
2021-03-05 18:09:03 +0100

    font fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
   def home
     @words = Word.all
     if params[:word]
-      @words = @words.select { |word| word.name.start_with?(params[:word]) }
+      @keyword = params[:word].downcase
+      @words = Word.all.where("lower(name) LIKE :word", word: "%#{@keyword}%")
+      # @words = @words.select { |word| word.name.start_with?(params[:word]) }
     end
   end
 end
